086fd19f0d01bf03b6f2d99fe938a057e2513495
Liu Jia <15606167742@163.com>
2021-07-08 07:57:27 +0800

    初始化

---
+++ src/components/TopMenu.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="top-menu">
+		<el-image class="logo" :src="require('@/assets/img/home/logo.png')" fit="contain"></el-image>
+		<div>你好世界 HelloWord</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TopMenu',
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style scoped lang="scss">
+.top-menu {
+	position: sticky;
+	height: 118px;
+	display: flex;
+	align-items: center;
+	.logo{
+		height: 50px;
+	}
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="home"><top-menu></top-menu></div>
+</template>
+
+<script>
+import TopMenu from '@/components/TopMenu.vue';
+
+export default {
+	name: 'Home',
+	components: {
+		TopMenu
+	}
+};
+</script>
