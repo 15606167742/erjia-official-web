e4569c6775dfe23505069bad3edd2a3bad789eaa
liujia <https://exmail.qq.com/login>
2021-07-08 19:59:43 +0800

    头部导航

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,6 @@
 	<div id="app"><router-view /></div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+	@import url('~@/assets/css/base.css');
+</style>
--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,27 +1,150 @@
 <template>
 	<div class="top-menu">
 		<el-image class="logo" :src="require('@/assets/img/home/logo.png')" fit="contain"></el-image>
-		<div>你好世界 HelloWord</div>
+		<div class="tabs">
+			<div class="tab" :class="{active: $route.path==tab.path}" :key="tab.path" v-for="tab in tabs"
+				@click="gotoPath(tab.path)">{{tab.text}}</div>
+		</div>
+		<i class="el-icon-menu my-menu" @click="showMenu=true"></i>
+		<el-drawer :visible="showMenu" direction="rtl" :with-header="false" :show-close="false" :size="200"
+			:modal-append-to-body="false" :before-close="handleClose">
+			<el-menu :default-active="activeMenu" background-color="#FFF" text-color="#333"
+				active-text-color="var(--color-t-active)" @select="gotoPath">
+				<el-menu-item :index="tab.path" :key="tab.path" v-for="tab in tabs">
+					<i class="el-icon-document"></i>
+					<span slot="title">{{tab.text}}</span>
+				</el-menu-item>
+			</el-menu>
+		</el-drawer>
 	</div>
 </template>
 
 <script>
-export default {
-	name: 'TopMenu',
-	data() {
-		return {};
-	}
-};
+	export default {
+		name: 'TopMenu',
+		data() {
+			return {
+				tabs: [{
+						path: '/home',
+						text: '首页'
+					},
+					{
+						path: '/comein',
+						text: '走进尔家'
+					},
+					{
+						path: '/product',
+						text: '产品介绍'
+					},
+					{
+						path: '/service',
+						text: '运营服务'
+					},
+					{
+						path: '/info',
+						text: '信息中心'
+					},
+					{
+						path: '/cooperation',
+						text: '商务合作'
+					},
+					{
+						path: '/connect',
+						text: '联系我们'
+					},
+				],
+				showMenu: false,
+				activeMenu: '/home',
+			};
+		},
+		mounted() {},
+		methods: {
+			gotoPath(path) {
+				console.log(path)
+				if (this.$route.path != path) {
+					this.$router.push({
+						path
+					})
+				}
+			},
+			handleClose(done) {
+				done();
+				this.showMenu = false;
+			}
+		}
+	};
 </script>
 
 <style scoped lang="scss">
-.top-menu {
-	position: sticky;
-	height: 118px;
-	display: flex;
-	align-items: center;
-	.logo{
-		height: 50px;
+	.top-menu {
+		position: sticky;
+		height: 118px;
+		padding: 0 80px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		.logo {
+			height: 50px;
+		}
+
+		.tabs {
+			font-size: 2rem;
+			color: #333;
+			letter-spacing: 0.2rem;
+			display: flex;
+			align-items: flex-end;
+			flex-wrap: wrap;
+
+			.tab {
+				margin: 0 15px;
+				cursor: pointer;
+
+				&.active {
+					font-size: 2.5rem;
+					color: var(--color-t-active);
+				}
+
+				&:hover {
+					font-size: 2.5rem;
+					color: var(--color-t-active);
+					transition-duration: 1s;
+				}
+			}
+		}
+
+		.my-menu {
+			font-size: 3rem;
+			color: var(--color-t-active);
+			cursor: pointer;
+		}
 	}
-}
+
+	@media screen and (max-width: 1080px) {
+		.top-menu {
+			padding: 0 40px;
+
+			.tabs {
+				display: none;
+			}
+
+			.my-menu {
+				display: block;
+			}
+		}
+	}
+
+	@media screen and (min-width: 1080px) {
+		.top-menu {
+			padding: 0 80px;
+
+			.tabs {
+				display: flex;
+			}
+
+			.my-menu {
+				display: none;
+			}
+		}
+	}
 </style>
