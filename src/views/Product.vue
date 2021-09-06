31db4b69462fa13f4878294274349378adbd6e7b
liujia <https://exmail.qq.com/login>
2021-09-06 17:09:59 +0800

    大调整-产品介绍部分

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -3,44 +3,51 @@
 		<Header></Header>
 		<top-banner :info="info" @change="handleChange"></top-banner>
 		<div class="content1" v-if="activeTab === 1">
-			<div class="introduction">
+			<div class="institute">
 				<div class="row1">
-					<span class="row1_l">商务</span>
-					<span class="row1_r">中心</span>
+					<span class="row1_l">研究院</span>
+					<span class="row1_r">介绍</span>
 					<el-image class="row1_split" :src="require('@/assets/img/product/line.png')" fit="contain"></el-image>
 				</div>
-				<div class="row2">Business management</div>
-			</div>
-			<div class="bg"></div>
-			<div class="box1">
-				<div class="title">
-					<div class="title-row1">
-						Beyond the
-						<br />
-						apartment
+				<div class="row2">Introduction to the Institute</div>
+				<div class="introduction">
+					{{institute.introduction}}
+				</div>
+				<div class="line" :key="index" v-for="(tip, index) in institute.tips">
+					<div class="graph">
+						<div class="g g1"></div>
+						<div class="g g2"></div>
+						<div class="g g3"></div>
+						<div class="num">{{tip.num}}</div>
 					</div>
-					<div class="title-row2">不只是公寓</div>
-				</div>
-				<el-image class="box1-img" :src="imgResource + '/product/highlight1.png'" fit="contain"></el-image>
-			</div>
-			<div class="box2">
-				<el-image class="box2-img" :src="imgResource + '/product/highlight2.png'" fit="contain"></el-image>
-				<div class="text">
-					尔家全系公寓产品以Box设计理念、立体集成化原则，严格执行人体工程学原理，为居住者定制最舒适的家居尺度、设计最合理的收纳空间，使公寓产品回归居住本质，营造引领时代的生活空间和生活方式！
+					<div class="text">
+						{{tip.text}}
+					</div>
 				</div>
 			</div>
-			<div class="box2 sp">
-				<el-image class="box2-img" :src="imgResource + '/product/highlight3.png'" fit="contain"></el-image>
-				<div class="text">
-					尔家全系公寓产品以Box设计理念、立体集成化原则，严格执行人体工程学原理，为居住者定制最舒适的家居尺度、设计最合理的收纳空间，使公寓产品回归居住本质，营造引领时代的生活空间和生活方式！
+			<div class="advantage">
+				<div class="row1">
+					<span class="row1_l">四大产品</span>
+					<span class="row1_r">优势</span>
+					<el-image class="row1_split" :src="require('@/assets/img/product/line.png')" fit="contain"></el-image>
 				</div>
-			</div>
-			<div class="box1 sp">
-				<div class="title">
-					<div class="title-row1">Check in with bags</div>
-					<div class="title-row2">全装全配 拎包入住</div>
+				<div class="row2">Four product advantages</div>
+				<div class="box" :key="index" v-for="(advantage, index) in advantages">
+					<div class="content">
+						<div class="line1">
+							<div class="num">
+								{{advantage.num}}
+							</div>
+							<div class="title">
+								{{advantage.title}}
+							</div>
+						</div>
+						<div class="line2">
+							{{advantage.content}}
+						</div>
+					</div>
+					<el-image class="my-img" :src="advantage.img" fit="contain"></el-image>
 				</div>
-				<el-image class="box1-img" :src="imgResource + '/product/highlight4.png'" fit="contain"></el-image>
 			</div>
 		</div>
 		<div class="content2" v-else-if="activeTab === 2">
@@ -127,6 +134,53 @@ export default {
 				]
 			},
 			activeTab: 1,
+			institute: {
+				introduction: '作为国内高端服务是公寓领军品牌\n上海尔家率先成立国内首家公寓主题产品设计研究院，力邀清华，同济等国内一线学府的杰出设计人才加盟\n坚持制定设计方向专注打造业内领先的创新公寓产品。',
+				tips: [
+					{
+						num: '04',
+						text: '2020年全装全配+全屋智能+绿色环保+百变空间'
+					},
+					{
+						num: '03',
+						text: '2019年全装全配+全屋智能+绿色环保'
+					},
+					{
+						num: '02',
+						text: '2017年全装全配+智能家居'
+					},
+					{
+						num: '01',
+						text: '2016年全装全配'
+					},
+				]
+			},
+			advantages: [
+				{
+					num: '01',
+					title: '全装全配，拎包入住',
+					content: '集中定制化装修\n家具家电配备齐全\n提升居住品质，省去装修烦恼，\n降低装修成本，省力省时更省心。',
+					img: require('@/assets/img/product/advantage1.png'),
+				},
+				{
+					num: '02',
+					title: '百变空间，极致收纳',
+					content: '尔家产品以Box设计理念、立体集成化原则，\n严格执行人体工程学原理，\n为居住者定制最舒适的家居尺度、设计最合理的收纳空间，\n通过不同家居功能模块变化组合，\n快速实现一套居所多种场景的灵动切换。',
+					img: require('@/assets/img/product/advantage2.png'),
+				},
+				{
+					num: '03',
+					title: '智能家居，科技生活',
+					content: '小米智能家居解决方案深度介入\n个性化定制场景模式\n摆脱传统智能家居实体终端控制的束缚\n实现语音操控、人体感应，\n真智能、全自动的智能家居体验',
+					img: require('@/assets/img/product/advantage3.png'),
+				},
+				{
+					num: '04',
+					title: '健康环保，全面呵护',
+					content: '全屋装修主材严格执行绿色环保标准确保健康环保、安全无害的家居环境深度研发健康人居解决方案引领健康住宅未来发展方向。',
+					img: require('@/assets/img/product/advantage4.png'),
+				},
+			],
 			houseList1: [
 				{
 					id: 1,
@@ -251,33 +305,71 @@ export default {
 	overflow-x: hidden;
 
 	.content1 {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-around;
-		.box1 {
-			.title {
-				color: #666666;
-				.title-row1 {
-					font-size: 3.8rem;
-					line-height: 5rem;
-				}
-				.title-row2 {
-					font-size: 3rem;
-				}
-			}
-			.box1-img {
-				display: block;
+		.row1{
+			.row1_l{
+				color: #A9A9A9 !important;
 			}
 		}
-		.box2 {
-			.text {
-				font-size: 1.8rem;
-				line-height: 2.5rem;
-				color: #666666;
-				margin-top: 40px;
+		.row2{
+			color: #A9A9A9 !important;
+		}
+		.institute{
+			overflow: hidden;
+			background-color: #F2F2F2;
+			.introduction{
+				font-size: 1.6rem;
+				line-height: 3rem;
+				color: #333333;
+				text-align: center;
+				white-space: pre-line;
 			}
-			.box2-img {
-				display: block;
+			.line{
+				display: flex;
+				align-items: center;
+				.graph{
+					width: 5.196rem;
+					height: 5.999rem;
+					position: relative;
+					transform: translateX(50%);
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					.g{
+						width: 5.196rem;
+						height: 3rem;
+						position: absolute;
+						z-index: 1;
+						background-color: var(--color-t-active);
+					}
+					.g1{
+						transform: rotateZ(60deg);
+					}
+					.g2{
+						transform: rotateZ(120deg);
+					}
+					.num{
+						position: relative;
+						z-index: 2;
+						color: var(--color-t-white);
+						font-size: 3rem;
+						font-weight: bold;
+						line-height: 3rem;
+						
+					}
+				}
+				.text{
+					flex-grow: 1;
+					line-height: 5.999rem;
+					padding: 0 3rem;
+					background-color: #E5E5E5;
+					font-size: 2.4rem;
+					letter-spacing: 0.1rem;
+					color: #858585;
+					font-weight: bold;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				}
 			}
 		}
 	}
@@ -389,32 +481,13 @@ export default {
 @media screen and (max-width: 1080px) {
 	.product {
 		.content1 {
-			margin-bottom: 60px;
-			.bg {
-				display: none;
-			}
-			.box1 {
-				flex-basis: 100%;
-				.title {
-					text-align: center;
-					.title-row1 {
-						margin-top: 30px;
-					}
-					.title-row2 {
-						margin-top: 30px;
-					}
+			.institute{
+				padding-bottom: 30px;
+				.introduction{
+					margin: 30px 10px;
 				}
-				.box1-img {
-					margin-top: 30px;
-				}
-			}
-			.box2 {
-				flex-basis: 100%;
-				.box2-img {
-					margin-top: 30px;
-				}
-				.text {
-					padding: 20px;
+				.line{
+					margin: 50px 10px;
 				}
 			}
 		}
@@ -452,59 +525,17 @@ export default {
 @media screen and (min-width: 1080px) {
 	.product {
 		.content1 {
-			position: relative;
-			z-index: 0;
-			margin: 120px auto 60px;
-			width: 70%;
-			min-width: 1000px;
-			.bg {
-				position: absolute;
-				z-index: -1;
-				top: -60px;
-				width: 100vw;
-				height: 800px;
-				background-color: #fafafa;
-			}
-			.box1 {
-				margin-bottom: 100px;
-				flex-basis: 35%;
-				.title {
-					padding-right: 100px;
-					.title-row1 {
-						margin-top: 70px;
-					}
-					.title-row2 {
-						margin-top: 70px;
-						text-align: right;
-					}
+			.institute{
+				padding-bottom: 60px;
+				.introduction{
+					margin: 60px auto;
+					width: 50%;
+					min-width: 800px;
 				}
-				.box1-img {
-					margin-top: 150px;
-				}
-				&.sp {
-					flex-basis: 40%;
-					.title {
-						padding-right: 0;
-						.title-row2 {
-							text-align: center;
-						}
-					}
-					.box1-img {
-						margin-left: 50px;
-					}
-				}
-			}
-			.box2 {
-				margin-bottom: 100px;
-				flex-basis: 50%;
-				.text {
-					padding: 0 20px;
-				}
-				&.sp {
-					flex-basis: 40%;
-					.text {
-						padding: 0;
-					}
+				.line{
+					margin: 50px auto;
+					width: 50%;
+					min-width: 800px;
 				}
 			}
 		}
@@ -535,7 +566,7 @@ export default {
 		}
 	}
 	.row1 {
-		margin-top: 150px;
+		margin-top: 80px;
 	}
 }
 </style>
